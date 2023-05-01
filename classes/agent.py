d65fdcd6dc3e2fc2da7e82754acaa53d1804b0fc
Blindeen <kuba.szmajda@gmail.com>
2023-05-01 13:57:27 +0200

    adapt agents learn method to gui needs

--- classes/agent.py
+++ classes/agent.py
@@ -1,5 +1,8 @@
 import random
+import time
 import numpy as np
+
+from enums.colors import Colors
 
 
 class Agent:
@@ -43,14 +46,21 @@ class Agent:
         self.qtable[state][action] += \
             self.alfa * (reward + self.gamma * max(self.qtable[next_state]) - self.qtable[state][action])
 
-    def learn(self, episodes_number, environment):
+    def learn(self, episodes_number, environment, gui=False):
         for i in range(episodes_number):
             state = environment.reset()
+            if gui is not False:
+                environment.reset_gui()
+                time.sleep(0.5)
             done = False
 
             while not done:
                 action = self.get_action(state, environment.get_possible_actions())
                 next_state, reward, done = environment.step(action)
+                if next_state != state and gui is not False:
+                    environment.color_rectangle(Colors.WHITE.value, state)
+                    environment.color_rectangle(Colors.RED.value, next_state)
+                    time.sleep(0.1)
                 self.update(state, action, next_state, reward)
                 state = next_state
 
