a04c37cffd2fd43b0fe6d1be576625bbc8539d9e
Blindeen <kuba.szmajda@gmail.com>
2023-05-01 14:00:08 +0200

    adapt linear and square reinforcement learning scripts to gui needs

--- linear_reinforcment_learning.py
+++ linear_reinforcment_learning.py
@@ -2,13 +2,17 @@ from classes.agent import Agent
 from classes.linear_environment import LineEnvironment
 from enums.constants import Constants
 
+import pygame as p
+
 # Linear reinforcment learning
 qtable = {}
 episodes_number = 100000
 length = 6
 finish = 5
+surface = p.display.set_mode((600, 100))
 agent = Agent(Constants.ALFA.value, Constants.GAMMA.value, qtable)
-environment = LineEnvironment(length, finish)
+environment = LineEnvironment(length, finish, surface)
+environment.init_gui()
 
-agent.learn(episodes_number, environment)
+agent.learn(episodes_number, environment, True)
 agent.display_qtable()
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from classes.agent import Agent
 from classes.square_environment import SquareEnvironment
 from enums.constants import Constants
 
+import pygame as p
+
 # Square reinforcment learning
 qtable = {}
 episodes_number = 100000
 dimensions = (4, 4)
-finish_coordinates = (3, 3)
+finish_coordinates = (3, 1)
+surface = p.display.set_mode((400, 400))
 agent = Agent(Constants.ALFA.value, Constants.GAMMA.value, qtable)
-environment = SquareEnvironment(dimensions, finish_coordinates)
+environment = SquareEnvironment(dimensions, finish_coordinates, surface)
+environment.init_gui()
 
-agent.learn(episodes_number, environment)
+agent.learn(episodes_number, environment, True)
 agent.display_qtable()
